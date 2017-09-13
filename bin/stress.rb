036c05ba65ee95105722ae0a931e47d651148259
Rob Dickinson <robfromboulder@gmail.com>
2017-09-13 11:18:36 -0600

    Test kvtree vs blackhole

--- bin/stress.rb
+++ bin/stress.rb
@@ -1,14 +1,28 @@
 require '../lib/pmemkv/kv_engine'
 
-class Stress
+def test_engine(engine, count)
+  puts "\nTesting #{engine} engine..."
+  kv = KVEngine.new(engine, '/dev/shm/pmemkv', 1024 * 1024 * 1104)
 
-  kv = KVEngine.new('blackhole', '/dev/shm/stress', 1024 * 1024 * 1104)
-
+  puts "Putting #{count} sequential values"
   t1 = Time.now
-  6012298.times do |i|
+  count.times do |i|
     str = i.to_s
     kv.put(str, "#{str}!")
   end
-  puts "Put: #{Time.now - t1} sec"
+  puts "   in #{Time.now - t1} sec"
 
+  puts "Getting #{count} sequential values"
+  t1 = Time.now
+  failures = 0
+  count.times do |i|
+    str = i.to_s
+    failures += 1 if kv.get(str).nil?
+  end
+  puts "   in #{Time.now - t1} sec, failures=#{failures}"
 end
+
+count = 6012298
+test_engine('kvtree', count)
+test_engine('blackhole', count)
+puts "\nFinished!\n\n"
