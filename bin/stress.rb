dcb65f7d593fdc142e80b0d50771aaab09078871
Rob Dickinson <robfromboulder@gmail.com>
2017-09-13 10:31:20 -0600

    Clean up stress utility

--- bin/stress.rb
+++ bin/stress.rb
@@ -4,53 +4,11 @@ class Stress
 
   kv = KVEngine.new('blackhole', '/dev/shm/stress', 1024 * 1024 * 1104)
 
-  # kv.put('key1', 'value1')
-  # puts "!!!!!!!!!!!!!!!!!!>#{kv.get('key1')}<"
-  # # puts "!!!!!!!!!!!!!!!!!!>#{kv.get2('key1')}<"
-
   t1 = Time.now
   6012298.times do |i|
     str = i.to_s
     kv.put(str, "#{str}!")
   end
   puts "Put: #{Time.now - t1} sec"
-
-  # t1 = Time.now
-  # 6012298.times do |i|
-  #   str = i.to_s
-  #   kv.get(str)
-  # end
-  # puts "Get: #{Time.now - t1} sec"
-
-  #
-  # t1 = Time.now
-  # 6012298.times do |i|
-  #   str = i.to_s
-  #   kv.put2(str, "#{str}!")
-  # end
-  # puts "Put2: #{Time.now - t1} sec"
-
-  # t1 = Time.now
-  # 6012298.times do |i|
-  #   str = i.to_s
-  #   kv.put(str, "#{str}!")
-  # end
-  # puts "Put3: #{Time.now - t1} sec"
-
-  # t1 = Time.now
-  # 6012298.times do |i|
-  #   str = i.to_s
-  #   kv.put4(str, "#{str}!")
-  # end
-  # puts "Put4: #{Time.now - t1} sec"
-
-  # t1 = Time.now
-  # 6012298.times do |i|
-  #   str = i.to_s
-  #   kv.get(str)
-  # end
-  # puts "Get: #{Time.now - t1} sec"
-  #
-  # puts "123456 is #{kv.get('123456')}"
 
 end
