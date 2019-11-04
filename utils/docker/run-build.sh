b19bd34bab6bec4dde17d0da9fafc8470fe7aac9
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2019-11-04 09:53:56 +0100

    Minor tweaks in Travis files

--- utils/docker/run-build.sh
+++ utils/docker/run-build.sh
@@ -35,19 +35,19 @@
 #                                 pmemkv installed in given version
 #
 
-PREFIX=/usr
-
 set -e
 
 # build and install pmemkv
-cd ~
 pmemkv_version=$1
 cd /opt/pmemkv-$pmemkv_version/
 
-if [ "${OS}" = "ubuntu" ]; then
+if [ "${PACKAGE_MANAGER}" = "deb" ]; then
 	echo $USERPASS | sudo -S dpkg -i libpmemkv*.deb
-elif [ "${OS}" = "fedora" ]; then
+elif [ "${PACKAGE_MANAGER}" = "rpm" ]; then
 	echo $USERPASS | sudo -S rpm -i libpmemkv*.rpm
+else
+	echo "PACKAGE_MANAGER env variable not set or set improperly ('deb' or 'rpm' supported)."
+	exit 1
 fi
 
 echo
@@ -63,6 +63,5 @@ echo
 echo "################################################################"
 echo "### Checking an example"
 echo "################################################################"
-cd $WORKDIR
-cd examples
+cd $WORKDIR/examples
 PMEM_IS_PMEM_FORCE=1 ruby basic_example.rb
